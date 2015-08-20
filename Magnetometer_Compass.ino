283ba499de5b320b81467d86c7c6045e1c00f7ef
deloarts <deloarts@outlook.com>
2015-08-20 12:13:56 +0200

    Outsourced the "get angle" calculation

--- Magnetometer_Compass.ino
+++ Magnetometer_Compass.ino
@@ -1,8 +1,8 @@
 // ############################################################################################################################
 // ######################################### MAGNETOETER COMPASS ##############################################################
 // ############################################################################################################################
-#include <OLED_I2C_128x64_Monochrome.h>
-#include <Wire.h>
+#include <OLED_I2C_128x64_Monochrome.h>   // Own OLED display library
+#include <Wire.h>                         // Arduino I2C-BUS library
 
 #define HMC5883L_Address             0x1E // 7bit address of HMC5883L
 #define HMC5883L_Mode_Register       0x02
@@ -17,7 +17,6 @@ unsigned long Timer_Display = 0;
 struct HMC5883LAxisData
 {
     int X_Axis, Y_Axis, Z_Axis;
-    float Angle;
 } compass;
 // ############################################################################################################################
 // ######################################### SETUP ############################################################################
@@ -43,8 +42,8 @@ void setup()
 // ############################################################################################################################
 void loop()
 {
-    // Get the compass data
-    readHMC5883L();
+    // Get the calculated angle
+    float Angle = getAngle();
     // Send data depending on device and timer
     // Serial a bit slower than display
     if (millis() > Timer_Serial)
@@ -57,7 +56,7 @@ void loop()
         Serial.print(" | Z: ");
         Serial.print(compass.Z_Axis);
         Serial.print(" | Angle: ");
-        Serial.println(compass.Angle);
+        Serial.println(Angle);
         // Increase timer
         Timer_Serial = millis() + 500;
     }
@@ -70,14 +69,46 @@ void loop()
         lcd.printString("   ");
         lcd.printNumber(long(compass.Z_Axis), 10, 5);
         lcd.printString("   ");
-        lcd.printNumber(float(compass.Angle), 1, 10, 7);
+        lcd.printNumber(float(Angle), 1, 10, 7);
         lcd.printString("   ");
         // Increase timer
         Timer_Display = millis() + 100;
     }
 }
 // ############################################################################################################################
-// ######################################### WRITE COMPASS ####################################################################
+// ######################################### GET ANGLE ########################################################################
+// ############################################################################################################################
+float getAngle()
+{
+    // First of all get the raw data from the HMC5883L module
+    readHMC5883L();
+    /* 
+    Calculate the angle:
+        - angle (radiant) = atan(-y, x)
+        - angle (degrees) = angle (radiant) * (180 / PI)
+    */
+    float Angle = atan2(-compass.Y_Axis, compass.X_Axis) * (180 / M_PI);
+    /*
+    Calculate the declination. The formula for this is:
+        (DEGREE + (MINUTES / 60)) / (180 / PI);
+    You can get your declination on http://magnetic-declination.com/
+    Be careful, that you also set it positive or negative.
+
+    The declination for Vienna in Austria is +3°57', and so I set my variables
+    */
+    byte  Declination_Degree = +3;
+    byte  Declination_Minute = 57;
+    float Declination = (Declination_Degree + (Declination_Minute / 60)) / (180 / M_PI);
+    // Add the declination to the angle
+    Angle += Declination;
+    // Finally correct the angle, so it is shown from 0° to 360° (and not from -180° to +180°)
+    if (Angle < 0)
+        return Angle + 360;
+    else
+        return Angle;
+}
+// ############################################################################################################################
+// ######################################### WRITE HMC5883L ###################################################################
 // ############################################################################################################################
 void writeHMC5883L(int Parameter_Mode, int Parameter_Data)
 {
@@ -93,7 +124,7 @@ void writeHMC5883L(int Parameter_Data)
     Wire.endTransmission();
 }
 // ############################################################################################################################
-// ######################################### READ COMPASS #####################################################################
+// ######################################### READ HMC5883L ####################################################################
 // ############################################################################################################################
 void readHMC5883L()
 {
@@ -111,26 +142,6 @@ void readHMC5883L()
         compass.Z_Axis  = Wire.read() << 8 | Wire.read();
         compass.Y_Axis  = Wire.read() << 8 | Wire.read();
     }
-    /* 
-    Calculate the angle:
-        - angle (radiant) = atan(-y, x)
-        - angle (degrees) = angle (radiant) * (180 / PI)
-    */
-    compass.Angle = atan2(-compass.Y_Axis, compass.X_Axis) * (180 / M_PI);
-    /*
-    Calculate the declination. The formula for this is:
-        (DEGREE + (MINUTES / 60)) / (180 / PI);
-    You can get your declination on http://magnetic-declination.com/
-    Be careful, that you also set it positive or negative.
-
-    The declination for Vienna in Austria is +3°57', and so I set my variables
-    */
-    byte  Declination_Degree = +3;
-    byte  Declination_Minute = 57;
-    float Declination = (Declination_Degree + (Declination_Minute / 60)) / (180 / M_PI);
-    // Finally correct the angle, so it is shown from 0° to 360° (and not from -180° to +180°)
-    if (compass.Angle < 0)
-        compass.Angle += 360;
 }
 // ############################################################################################################################
 // ######################################### END OF CODE ######################################################################
